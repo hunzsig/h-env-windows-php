34d8d62d5258551d128d43815c7d0e478929ef5d
马志洋 <mazhiyang@shein.com>
2019-05-20 14:06:09 +0800

    rebuild

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # 完善配置好的一套php/web开发环境，适用于windows平台
 > 至于 linux 为什么不配，那是因为 linux 用命令安装本身已经极其方便（推荐CentOS）
-### 主体
+### 默认主体
 ```
-nginx、apache、php
+apache、php（5.6-7.3）
+```
+### 可选依赖服务
+#### 依赖服务都压缩放置于dependent，需要某个时，直接压缩到目录即可
+```
+nginx、redis、mysql、pgsql、elastic
 ```
 #### 开启拓展包含：
 ```
@@ -18,4 +23,4 @@ igbinary、redis、lzf、pdo_sqlsrv、sqlsrv、mongodb、amqp
 ```
 
 #### 建议clone到 **D:/Web**，改动最少
-> git clone git@github.com:hunzsig/h-web-env-windows.git D:/Web/h-web-env-windows
+> git clone git@github.com:hunzsig/h-web-env-windows.git D:\Web\h-web-env-windows
---
+++ webCtrl.bat
@@ -0,0 +1,130 @@
+====================================================
+@echo off
+rem 如出现问题请检查路径或尝试以管理员身份运行
+
+echo ==================begin========================
+
+cls 
+
+SET XDISK=D:
+SET XPATH=%XDISK%\Web\h-web-env-windows\php_7.3
+SET RunHiddenConsole=%XPATH%\RunHiddenConsole
+SET APACHE_DIR=%XPATH%\Apache24\bin\
+SET NGINX_DIR=%XPATH%\nginx-1.15.10\
+SET REDIS_DIR=%XPATH%\Redis-x64-3.2.100\
+SET REDIS_DIR=%XPATH%\Redis-x64-3.2.100\
+SET REDIS_DIR=%XPATH%\Redis-x64-3.2.100\
+
+color ff 
+TITLE ANPR 控制面板
+
+CLS 
+ECHO.# by hunzsig 20190507
+ECHO %~0
+
+:MENU
+
+ECHO.----------------------进程列表----------------------
+tasklist|findstr /i "nginx.exe"
+tasklist|findstr /i "httpd.exe"
+tasklist|findstr /i "redis-server.exe"
+ECHO.----------------------------------------------------
+ECHO.[1] 启动/重启[ANPR模式]
+ECHO.[2] 启动/重启[APR模式]
+ECHO.[9] 关闭
+ECHO.[0] 退出 
+ECHO.输入功能号:
+
+set /p ID=
+IF "%id%"=="1" GOTO startANPR
+IF "%id%"=="2" GOTO startAPR
+IF "%id%"=="9" GOTO stop 
+IF "%id%"=="0" EXIT
+PAUSE 
+
+
+
+:startANPR 
+call :shutdown
+call :startApache
+call :startNginx
+call :startRedis
+GOTO MENU
+
+:startAPR 
+call :shutdown
+call :startApache
+call :startRedis
+GOTO MENU
+
+:stop 
+call :shutdown
+GOTO MENU
+
+:shutdown
+call :shutdownRedis
+call :shutdownNginx
+call :shutdownApache
+goto :eof
+
+:shutdownNginx
+ECHO.Stopping Nginx...... 
+taskkill /F /IM nginx.exe > nul
+ECHO.OK
+ECHO. 
+goto :eof
+
+:startNginx
+ECHO.Start Nginx...... 
+IF NOT EXIST "%NGINX_DIR%nginx.exe" (
+ECHO "%NGINX_DIR%nginx.exe" not exist
+goto :eof
+)
+%XDISK% 
+cd "%NGINX_DIR%" 
+start nginx.exe
+ECHO.OK
+ECHO.
+goto :eof
+
+
+:shutdownApache
+ECHO.Stopping Apache...... 
+taskkill /F /IM httpd.exe > nul
+ECHO.OK
+ECHO. 
+goto :eof
+
+:startApache
+ECHO.Start Apache...... 
+IF NOT EXIST "%APACHE_DIR%httpd.exe" (
+ECHO "%APACHE_DIR%httpd.exe" not exist
+goto :eof
+)
+%XDISK% 
+cd "%APACHE_DIR%" 
+%RunHiddenConsole% httpd.exe
+ECHO.OK
+ECHO.
+goto :eof
+
+:shutdownRedis
+ECHO.Stopping Redis...... 
+taskkill /F /IM redis-server.exe > nul
+ECHO.OK
+ECHO. 
+goto :eof
+
+:startRedis
+ECHO.Start Redis...... 
+IF NOT EXIST "%REDIS_DIR%redis-server.exe" (
+ECHO "%REDIS_DIR%redis-server.exe" not exist
+goto :eof
+)
+%XDISK% 
+cd "%REDIS_DIR%" 
+%RunHiddenConsole% redis-server.exe
+ECHO.OK
+ECHO.
+goto :eof
+
